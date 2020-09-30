dea9427decc4c63904a81e3914f23fc4dee0c9a0
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-09-30 12:42:39 +0700

    mongo connect fix typo

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -4,12 +4,7 @@ const { MONGO_USER, MONGO_PASS, MONGO_DB } = process.env;
 
 const mongoURI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.h3ocx.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
 
-try {
-  mongoose.connect(
-    mongoURI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log("mongo connected !!")
-  );
-} catch (err) {
-  console.log(err);
-}
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongo connected"))
+  .catch((err) => console.log(err));
