08b4fdbbf553e7a227e0b8ffe6ad1b42297f554a
Kim Taeyeon <teerapatpor@gmai.com>
2020-10-02 17:05:38 +0700

    add schema

--- src/database/model/petitionStatus.js
+++ src/database/model/petitionStatus.js
@@ -3,3 +3,20 @@ exports.voting = "กำลังโหวต";
 exports.waiting_for_approved = "กำลังรอการนำไปสู่การอนุมัติ";
 exports.waiting_for_voting = "กำลังรอการนำไปสู่การโหวต";
 exports.reject = "ปฏิเสธ";
+const petition = require("./petition");
+
+exports.updateStatus = async (petitinId, status) => {
+  try {
+    const result = await petition.updateOne(
+      { _id: petitinId },
+      {
+        $set: {
+          status,
+        },
+      }
+    );
+    sendSuccessResponse(res, { result });
+  } catch (error) {
+    sendSuccessResponse(res, error);
+  }
+};
