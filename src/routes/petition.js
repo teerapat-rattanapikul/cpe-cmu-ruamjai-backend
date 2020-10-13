6566063b29cb2aa92aa4de567e55578b5db35cc2
AoFNontawat <Nontawatt.udprom@gmail.com>
2020-10-13 17:04:28 +0700

    search

--- src/routes/petition.js
+++ src/routes/petition.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 const controllers = require("../controllers");
 
 router.get("/", controllers.petitionController.getAllPetitionsVoting);
-router.get("/:id", controllers.petitionController.findPetitionById);
 router.get("/status", controllers.petitionController.getAllStatus);
 router.get("/recent", controllers.petitionController.getRecentPetitions);
-router.post("/filter", controllers.petitionController.filterPetitions);
-router.get("/type", controllers.petitionController.getPetitionType);
 router.get("/trending", controllers.petitionController.getTrendingPetitions);
+router.get("/search", controllers.petitionController.getSearch);
+router.post("/myPetitions", controllers.petitionController.getMyPetitions);
+router.post("/add", controllers.petitionController.addPetition);
+router.post("/vote", controllers.petitionController.votePetition);
+router.post("/filter", controllers.petitionController.filterPetitions);
+
+router.post("/approved", controllers.petitionController.approveForvote);
+//router.post("/finalApprove");
+router.post("/reject", controllers.petitionController.rejectPetition);
 module.exports = router;
