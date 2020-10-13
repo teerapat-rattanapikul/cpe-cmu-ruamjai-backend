530c241208cef0b6da38aee33972667344bdd3bf
Kim Taeyeon <teerapatpor@gmai.com>
2020-10-13 19:52:47 +0700

    change post to get

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const verify = require("../middlewares/verifyJWT");
 router.post("/login", controllers.userController.login);
 router.post("/approved", verify(), controllers.userController.approveForvote);
 router.post("/finalApprove", verify(), controllers.userController.finalApprove);
-router.post("/my", verify(), controllers.userController.getMyProfile);
+router.get("/my", verify(), controllers.userController.getMyProfile);
 router.post("/reject", verify(), controllers.userController.rejectPetition);
 router.post("/vote", verify(), controllers.userController.votePetition);
 router.post("/add", verify(), controllers.userController.addPetition);
