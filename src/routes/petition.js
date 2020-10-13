4b06e070f06de5f1d5d943511829d67bd4a83908
AoFNontawat <Nontawatt.udprom@gmail.com>
2020-10-13 11:44:55 +0700

    treningPetition

--- src/routes/petition.js
+++ src/routes/petition.js
@@ -4,6 +4,7 @@ const controllers = require("../controllers");
 router.get("/", controllers.petitionController.getAllPetitionsVoting);
 router.get("/status", controllers.petitionController.getAllStatus);
 router.get("/recent", controllers.petitionController.getRecentPetitions);
+router.get("/trending", controllers.petitionController.getTrendingPetitions);
 router.post("/myPetitions", controllers.petitionController.getMyPetitions);
 router.post("/add", controllers.petitionController.addPetition);
 router.post("/vote", controllers.petitionController.votePetition);
